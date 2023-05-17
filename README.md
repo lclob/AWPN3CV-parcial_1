99433b97ba829e7adaa44db50d7f2199096d5b71
lclob <116601056+lclob@users.noreply.github.com>
2023-05-17 01:16:08 -0300

    update: styles.css

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # DW3CV-AWP
 Aplicaciones Web Progresivas - Entrevista Laboral - Parcial I - Lautaro Climent
+
+paleta de colores - https://coolors.co/f6d8ae-cee461-dddddd-083d77-da4167
--- main.js
+++ main.js
@@ -20,14 +20,14 @@ buttonSearch.addEventListener('click', event => {
       console.log(query)
       return query;
     })
-    .then(async function(query){
+    .then(async function (query) {
       let page_num = 1;
       const res = await fetch(`https://api.pexels.com/v1/search?query=${query}&page=${page_num}`, {
-          method: "GET",
-          headers: {
-              Accept: "application/json",
-              Authorization: APIPexels, 
-          },
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          Authorization: APIPexels,
+        },
       });
 
       const image = await res.json();
@@ -65,25 +65,23 @@ function placeInfo(data) {
   div.classList.add("card", "mb-3");
   resultElement.appendChild(div);
   div.innerHTML = `
-
-    <div class="row g-0">
+      <div class="row g-0">
       <div class="col-md-4">
         <img src="" class="img-fluid imagen w-100 h-100 rounded-start" alt="..." style="object-fit:cover"/>
       </div>
       <div class="col-md-8">
         <div class="card-body">
-          <h5 class="card-title">${name}</h5>
+          <h2 class="card-title badge bg-primary rounded-pill p-3">${name}</h2>
           <ul class="list-group">
-            <li class="list-group-item fw-semibold">Lugar: <span class="fw-normal">${name}</span></li>
-            <li class="list-group-item fw-semibold">Temperatura: <span class="fw-normal">${temp}°C</span></li>
-            <li class="list-group-item fw-semibold">Latitud :<span class="fw-normal">${lat}</span></li>
-            <li class="list-group-item fw-semibold">Longitud: <span class="fw-normal">${lon}</span></li>
-            <li class="list-group-item fw-semibold">Temp. máxima: <span class="fw-normal">${tempMax}°C</span></li>
-            <li class="list-group-item fw-semibold">Temp. mínima: <span class="fw-normal">${temMin}°C</span></li>
-            <li class="list-group-item fw-semibold">Humedad: <span class="fw-normal">${humedad}</span></li>
-            <li class="list-group-item fw-semibold">Sensación Térmica: <span class="fw-normal">${st}</span></li>
-            <li class="list-group-item fw-semibold">Presión Atmosférica: <span class="fw-normal">${pa}</span></li>
-            <li class="list-group-item fw-semibold">Viento: <span class="fw-normal">${wind}</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Temperatura: <span class="badge bg-primary rounded-pill p-2">${temp} °C</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Latitud:<span class="badge bg-primary rounded-pill p-2">${lat}</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Longitud: <span class="badge bg-primary rounded-pill p-2">${lon}</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Temp. máxima: <span class="badge bg-primary rounded-pill p-2">${tempMax} °C</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Temp. mínima: <span class="badge bg-primary rounded-pill p-2">${temMin} °C</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Humedad: <span class="badge bg-primary rounded-pill p-2">${humedad} g/m³.</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Sensación Térmica: <span class="badge bg-primary rounded-pill p-2">${st} °C</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Presión Atmosférica: <span class="badge bg-primary rounded-pill p-2">${pa} hPa</span></li>
+            <li class="list-group-item d-flex justify-content-between align-items-center fw-semibold">Viento: <span class="badge bg-primary rounded-pill p-2">${wind} m/s</span></li>
           </ul>
         </div>
       </div>
@@ -92,9 +90,9 @@ function placeInfo(data) {
 
 };
 
-function setImage(photo){
- let imagen = document.querySelector('.imagen');
- imagen.src = "";
- imagen.src = photo;
+function setImage(photo) {
+  let imagen = document.querySelector('.imagen');
+  imagen.src = "";
+  imagen.src = photo;
 }
 
